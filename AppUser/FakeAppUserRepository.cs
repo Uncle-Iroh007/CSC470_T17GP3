faf2862c30fd145b33dc6baa43ab4995004df368
Naluca <43424992+Naluca@users.noreply.github.com>
2020-09-16 21:21:57 -0500

    Added AppUser files

---
+++ AppUser/FakeAppUserRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P3_Code
+{
+    public class FakeAppUserRepository : IAppUser
+    {
+        private static Dictionary<int, AppUser> Users;
+
+        public FakeAppUserRepository()
+        {
+
+            if(Users == null)
+            {
+                Users = new Dictionary<int, AppUser>();
+
+                Users.Add(1, new AppUser
+                {
+                    UserName = "Naulca",
+                    Password = "password",
+                    FirstName = "Raquel",
+                    LastName = "Meyer",
+                    EmailAddress = "raquel.meyer@trojans.dsu.edu",
+                    IsAuthenticated = true
+
+                    
+                    
+                });
+                Users.Add(2, new AppUser
+                {
+                    UserName = "AnnaF",
+                    Password = "password",
+                    FirstName = "Anna",
+                    LastName = "Fields",
+                    EmailAddress = "anna.fields@trojans.dsu.edu",
+                    IsAuthenticated = true
+
+
+
+                });
+                Users.Add(3, new AppUser
+                {
+                    UserName = "Iroh007",
+                    Password = "password",
+                    FirstName = "Sandesh",
+                    LastName = "Sharma",
+                    EmailAddress = "sandesh.sharma@trojans.dsu.edu",
+                    IsAuthenticated = true
+
+
+
+                });
+            }
+
+        }
+
+        public bool Login(string Username, string Password)
+        {
+           
+        }
+        public List<AppUser> GetAll()
+        {
+            
+        }
+        public void SetAuthentication(string Username, bool IsAuthenticated)
+        {
+
+        }
+      
+        
+
+        public AppUser GetByUserName(string UserName)
+        {
+
+        }
+    }
+}
